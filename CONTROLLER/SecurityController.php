6884c9bdba1b39f9f4f32a9dcedad1f7f0dbf3ed
Tom Campion <tomcampion10@laposte.net>
2019-08-12 17:28:54 +0200

    authentification

--- CONTROLLER/SecurityController.php
+++ CONTROLLER/SecurityController.php
@@ -23,9 +23,9 @@ class SecurityController extends Controller{
         if(strlen($password) < 3)
             $message = '<p style="color:#db2727;">Votre mot de passe est trop court, il doit faire au moins 3 caractère </p>';
         if(!$this->helper->isAlpha($firstname))
-            $message = $message.'<p style="color:#db2727;">Le champ nom ne peut contenir que des lettres ! </p>';
-        if(!$this->helper->isAlpha($lastname))
             $message = $message.'<p style="color:#db2727;">Le champ prénom ne peut contenir que des lettres ! </p>';
+        if(!$this->helper->isAlpha($lastname))
+            $message = $message.'<p style="color:#db2727;">Le champ nom ne peut contenir que des lettres ! </p>';
         if(!$this->userManager->isEmailUnique($email))
             $message = $message.'<p style="color:#db2727;">Cette adresse e-mail est déjà utilisé </p>';
         if(!$this->helper->isEmail($email))
@@ -56,6 +56,38 @@ class SecurityController extends Controller{
         return $message;
     }
 
+    private function login(String $email, String $password)
+    {
+        $message = '';
+
+        if(strlen($email) > 254)
+            $message = '<p style="color:#db2727;">La taille maximum de l\'email est de 254 caractère ! </p>';
+        if(strlen($password) > 254)
+            $message = $message.'<p style="color:#db2727;">La taille maximum du mot de passe est de 254 caractère ! </p>';
+
+        if (empty($message)){
+            $user = $this->userManager->userExist($email, $password);
+            if( !empty($user) ){
+                foreach ($user as $key => $value) {
+                    if($key != 'password'){
+                        $_SESSION[$key]=$value;
+                    }
+                }
+                header('Location: profil');
+            }else{
+                $message = $message.'<p style="color:#db2727;">Vos identifiants ne sont pas reconnus, vérifiez l’adresse mail et le mot de passe saisis</p>';
+            }
+        }
+        return $message;
+    }
+
+    public function executeLogout()
+    {
+        session_unset();
+        session_destroy();
+        header('Location: accueil');
+    }
+
     public function executeConnexion(){
         echo  $this->twig->render('connexion.twig');
     }
@@ -69,6 +101,17 @@ class SecurityController extends Controller{
 
             $msg_register = $this->register($_POST['prenom'], $_POST['nom'], $_POST['register_email'], $_POST['register_password']);
             echo $this->twig->render('connexion.twig', ['message_register' => $msg_register]);
+        }else{
+            $this->executeConnexion();
+        }
+    }
+
+    public function executeAuthentification()
+    {
+        if (!empty($_POST['email']) and !empty($_POST['password'])) {
+
+            $msg_login = $this->login($_POST['email'], $_POST['password']);
+            echo $this->twig->render('connexion.twig', ['message_connexion' => $msg_login]);
         }else{
             $this->executeConnexion();
         }
