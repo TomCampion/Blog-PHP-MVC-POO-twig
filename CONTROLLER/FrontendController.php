8b0a575a7b90982dbb27dd3c3012d3d382c47178
Tom Campion <tomcampion10@laposte.net>
2019-08-12 16:02:39 +0200

    creation of the FrontendController class

---
+++ CONTROLLER/FrontendController.php
@@ -0,0 +1,30 @@
+<?php
+namespace Tom\Blog\Controller;
+
+class FrontendController extends Controller{
+
+    public function executeAccueil(){
+        echo  $this->twig->render('accueil.twig');
+    }
+
+    public function executeA_propos(){
+        echo  $this->twig->render('a_propos.twig');
+    }
+
+    public function executeContact(){
+        echo  $this->twig->render('contact.twig');
+    }
+
+    public function executeCv(){
+        echo  $this->twig->render('cv.twig');
+    }
+
+    public function executePortfolio(){
+        echo  $this->twig->render('portfolio.twig');
+    }
+
+    public function executeProjet1(){
+        echo  $this->twig->render('projet1.twig');
+    }
+
+}
