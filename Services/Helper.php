05beea7ee3ec79ea50cb69b0dc81ad214ac2164a
Tom Campion <tomcampion10@laposte.net>
2019-08-12 16:57:20 +0200

    helper

---
+++ Services/Helper.php
@@ -0,0 +1,20 @@
+<?php
+class Helper{
+    public function isAlpha($str)
+    {
+        $valid = true;
+        $regex = preg_match('#^[\p{Latin}\' -]+$#u', $str);
+        if(!$regex){
+            $valid = false;
+        }
+        return $valid;
+    }
+    public function isEmail($field)
+    {
+        $valid = true;
+        if (!filter_var($field, FILTER_VALIDATE_EMAIL)) {
+            $valid = false;
+        }
+        return $valid;
+    }
+}
