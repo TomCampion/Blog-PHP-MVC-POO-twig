4c6bf882f3133c68a76b8e41a1ada96965cfc6da
Tom Campion <tomcampion10@laposte.net>
2019-08-18 11:46:24 +0200

    Creation of the Helper class

---
+++ Services/Helper.php
@@ -0,0 +1,25 @@
+<?php
+namespace Tom\Blog\Services;
+
+class Helper{
+
+    public function isAlpha($str)
+    {
+        $valid = true;
+        $regex = preg_match('#^[\p{Latin}\' -]+$#u', $str);
+        if(!$regex){
+            $valid = false;
+        }
+        return $valid;
+    }
+
+    public function isEmail($field)
+    {
+        $valid = true;
+        if (!filter_var($field, FILTER_VALIDATE_EMAIL)) {
+            $valid = false;
+        }
+        return $valid;
+    }
+
+}
