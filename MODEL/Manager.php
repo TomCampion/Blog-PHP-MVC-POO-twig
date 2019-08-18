b765da00574fec67f133ae6cb359a10b793f4834
Tom Campion <tomcampion10@laposte.net>
2019-08-18 10:32:25 +0200

    Creation of the Manager class

---
+++ MODEL/Manager.php
@@ -0,0 +1,24 @@
+<?php
+namespace Tom\Blog\Model;
+
+require 'config.php';
+
+class Manager
+{
+    protected $db;
+    function __construct() {
+        $this->dbConnect();
+        $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $this->db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
+    }
+
+    private function dbConnect()
+    {
+        try{
+            $this->db = new \PDO('mysql:host='.getenv("DB_HOST").';dbname='.getenv("DB_NAME").';charset=utf8', ''.getenv("DB_USER").'', ''.getenv("DB_PASSWORD").'');
+        }catch(Exception $e){
+            echo 'Echec de la connection à la base de donnée : '.$e->getMessage().'<br>';
+        }
+        return $this->db;
+    }
+}
